7c1be2de9b00746d99dde7d9cf7657b26d1a9d4f
Mousuf <Mousuf@LAPTOP-UHRV5T0E>
2020-07-20 19:21:01 +0100

    Hello World, First Attempt

--- helloWorld/src/helloWorld/Main.java
+++ helloWorld/src/helloWorld/Main.java
@@ -4,13 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	
 		
-		int a = 3;
-		int b = 4;
-		
-		int c = a + b;
-		
-		System.out.print(c);
+		System.out.print("Hello World!");
 
 	}
 
